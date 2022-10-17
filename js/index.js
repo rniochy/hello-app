733decfdf1729377b2643f747393d463cdccda44
rniochy <lniochy@gmail.com>
2022-10-17 23:58:29 +0100

    new features with js file

--- js/index.js
+++ js/index.js
@@ -4,21 +4,21 @@ const message = document.getElementById("message");
 const username = document.getElementById("username");
 const password = document.getElementById("password");
 const messageContainer = document.getElementById("message-container");
-const colorGrey = "ccc", colorRed = "red";
+const colorGrey = "ccc", colorRed = "ff0000";
 
 login.addEventListener('click', ()=>{
-    // if(!username.value && !password.value){
-    //     setBorderColor(username, colorRed);
-    //     setBorderColor(password, colorRed);
-    //     messageAlert("Fill the all field",0);
-    // }
+    if(!username.value && !password.value){
+        setBorderColor(username, colorRed);
+        setBorderColor(password, colorRed);
+        messageAlert("Fill the all field",0);
+    }
 
     if(!username.value){
-        setBorderColor(username, "red");
+        setBorderColor(username, colorRed);
       
         }
     if(!password.value){
-         password.style = "border-color: red";
+        setBorderColor(password, colorRed);
     }
 });
 
@@ -29,7 +29,7 @@ logout.addEventListener('click', ()=> {
 });
 
   function messageAlert(msg, position){
-    msg > 0 ? message.innerText = msg:'';
+    !Number.isInteger(msg) ? message.innerText = msg:'';
     messageContainer.style = `top: ${position}rem `;
   }
 
