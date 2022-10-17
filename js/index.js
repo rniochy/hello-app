705c0bbd8ffcde23ed1d863c07b0b82341d4ec49
rniochy <lniochy@gmail.com>
2022-10-17 23:12:14 +0100

    new fwatures with js file - event to login button

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
 const login = document.getElementById("login");
+const logout = document.getElementById("logout");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
 
 login.addEventListener('click', ()=>{
-     alert();
+     if(!username.value) username.style = "border-color: red";
+     if(!password.value) password.style = "border-color: red";
+});
+
+logout.addEventListener('click', ()=> {
+    username.style = "border-color: #ccc";
+    password.style = "border-color: #ccc";
 });
